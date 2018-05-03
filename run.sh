bad131c4feef5f9c05f80c1aed952f0f0ce8a985
juuso haapaniemi <juuso.haapaniemi@myy.haaga-helia.fi>
2018-05-03 15:08:42 +0300

    test 4

--- run.sh
+++ run.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-echo "1"
 set -o verbose
-
-echo "2"
 sudo apt-get update
 sudo apt-get -y install git salt-minion
 
-echo "3"
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/haapjuu/qwerty
-cd qwerty/
-./highstate.sh
-
-echo "4"
+sudo salt '*' state.apply mumble
 setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
 git config --global credential.helper "cache --timeout=3600"
 git config --global push.default simple
 # git config --global user.email "juuso.haapaniemi@myy.haaga-helia.fi"
 # git config --global user.name "juuso haapaniemi"
-
-echo "5"
-echo "Done."
+echo "OK!"
