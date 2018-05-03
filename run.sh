c37aaf826ad4b04071af8f0cd0a2c866c44ab74e
juuso haapaniemi <juuso.haapaniemi@myy.haaga-helia.fi>
2018-05-03 16:19:55 +0300

    it works 1

--- run.sh
+++ run.sh
@@ -2,17 +2,14 @@
 
 set -o verbose
 sudo apt-get update
-sudo apt-get -y install git salt-minion salt-master
-
+sudo apt-get -y install git salt-minion
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/haapjuu/qwerty
 cd qwerty/
 bash highstate.sh
 setxkbmap fi
-sudo apt-get -y purge salt-master
 sudo timedatectl set-timezone Europe/Helsinki
 git config --global credential.helper "cache --timeout=3600"
 git config --global push.default simple
 git config --global user.email "juuso.haapaniemi@myy.haaga-helia.fi"
 git config --global user.name "juuso haapaniemi"
-echo "OK!"
